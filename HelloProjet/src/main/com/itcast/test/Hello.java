249a9580c69d73dd43241e352f7160f94e80e74b
wangyangkun <1090052547@qq.com>
2019-06-29 18:38:15 +0800

    把本地添加到git

--- HelloProjet/src/main/com/itcast/test/Hello.java
+++ HelloProjet/src/main/com/itcast/test/Hello.java
@@ -4,5 +4,9 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("itcast.test.Hello");
 		System.out.println("哈哈哈哈");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("HelloWorld");
+        }
+
 	}
 }
